8885d28aee31c0d13324e27cb12539c71160ef12
Bewtaich <bewtaich@gmail.com>
2024-09-30 10:28:49 -0500

    Added last thought controllers

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,3 +1,4 @@
+const { validateHeaderName } = require("http");
 const { Thought, User } = require("../models");
 
 const getAllThoughts = async (req,res)=>{
@@ -35,6 +36,69 @@ const createThought = async (req,res) => {
         res.status(500).json(err);
     }
 }
+
+const updateThought=async(req,res)=>{
+    try{
+        const thought = await Thought.findByIdAndUpdate(req.params.id, req.body, {
+            runValidators:true,
+            new:true,
+        });
+        if (!thought){
+            return res.status(404).json({message:'No though found with this ID'});
+        }
+        res.json(thought);
+    } catch (err) {
+        res.status(500).json(err)
+    }
+}
+const deleteThought = async (req,res)=>{
+    try{
+        const thought = await Thought.findByIdAndDelete(req.params.id);
+        if(!thought){
+            return res.status(404).json({message:'No though found with this ID'});
+        }
+    await User.findOneAndUpdate(
+        {thoughts:req.params.id},
+        {$pull:{thoughts:req.params.id}},
+        {new:true}
+    );
+    res.json({message: 'Thought deleted'})
+    } catch (err) {
+        res.status(500).json(err);
+    }
+};
+
+const addReaction = async (req,res)=>{
+    try{
+        const thought = await Thought.findByIdAndUpdate(
+            req.params.thoughtId,
+            {$addToSet:{reactions:req.body}},
+            {new:true}
+        );
+        if(!thought){
+            return res.status(404).json({message:'No though found with this ID'});
+        }
+        res.json(thought);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+};
+
+const removeReaction = async (req,res)=>{
+    try{
+        const thought = await Thought.findByIdAndUpdate(
+            req.params.thoughtId,
+            {$pull:{reactions:{reactionId:req.params.reactionId}}},
+            {new:true}
+        );
+        if(!thought){
+            return res.status(404).json({message:'No though found with this ID'});
+        }
+        res.json(thought);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+};
 
 module.exports = {
   getAllThoughts,
